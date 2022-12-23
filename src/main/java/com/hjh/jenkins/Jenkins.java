d0f89c07d164c8af4cc4451c071f65deabc385ba
hutFire <2603867986@qq.com>
2022-12-23 14:45:32 +0800

    feat 初始化

---
+++ src/main/java/com/hjh/jenkins/Jenkins.java
@@ -0,0 +1,20 @@
+package com.hjh.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Hjh
+ * @Date 2022/12/23 13:17
+ * 认真写代码，细节决定成败
+ * Dignity is only above the sword
+ */
+
+@SpringBootApplication
+public class Jenkins {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkins.class, args);
+    }
+
+}
---
+++ src/main/java/com/hjh/jenkins/controller/JenkinsController.java
@@ -0,0 +1,23 @@
+package com.hjh.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Hjh
+ * @Date 2022/12/23 13:29
+ * 认真写代码，细节决定成败
+ * Dignity is only above the sword
+ */
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "test";
+    }
+
+}
